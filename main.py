6caf93e01ef1599e9bac133a3c2b395c95f863d2
AndrewsOroga <143944433+AndrewsOroga@users.noreply.github.com>
2023-10-01 16:34:34 +0300

    Add files via upload
    Список Учнів

---
+++ main.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('L1.db')
+
+cursor = conn.cursor()
+score = 9
+cursor.execute('''SELECT * FROM students WHERE avg_score > (?)''', [score])
+
+data = cursor.fetchall()
+
+for i in range(0, len(data)):
+    print(data[i])
+    
+conn.commit()
